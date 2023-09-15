2ee26f0729501c5b0d835e188bdae6ee61bf0ee9
Juskinbo <juskinbo@outlook.com>
2023-09-16 02:43:04 +0800

    fix: fix admin cannot get visitor info

--- db/adminDB.js
+++ db/adminDB.js
@@ -3,7 +3,7 @@ const pool = require("./pool");
 module.exports = {
   // 获取所有人信息时候不包括本人信息
   get_all_info(unionid) {
-    const sql = `select unionid, name, phone_number, room_id, skills, willingness, type from user_info where unionid != ?`;
+    const sql = `select unionid, name, phone_number, room_id, skills, willingness, type from user_info where unionid != ? or unionid != null`;
     return pool.execute(sql, [unionid]);
   },
   admin_change_info(info) {
